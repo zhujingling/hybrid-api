ab497ae367a2b59c2af73e1477246af0f50d9a6e
tanbo <464328895@qq.com>
2017-11-06 14:29:54 +0800

    新加用户认证及测试页面

---
+++ config/webpack.lib.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const globalConfig = require('../global.config');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        app: path.resolve(globalConfig.appPath, 'libraries/index.ts'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        path: globalConfig.buildPath,
+        filename: './hybrid-api.js',
+        libraryTarget: 'umd',
+        library: 'YBB',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            enforce: 'pre',
+            use: [{
+                loader: 'tslint-loader',
+                options: {
+                    configuration: require('../tslint.json'),
+                    emitErrors: false,
+                    failOnHint: false,
+                    formatter: 'tslint-formatter-eslint-style'
+                }
+            }]
+        }, {
+            test: /\.ts$/,
+            use: ['awesome-typescript-loader']
+        }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'ENV': JSON.stringify(ENV)
+            }
+        })
+    ]
+};
